7427e24d1780744fa6544c8da176ff23b5a7cf4d
orest-cloud <orest.szopiak@gmail.com>
2021-04-04 23:18:01 -0400

    Added home page bio

---
+++ src/components/circle-button/circle-button.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+// Images
+import CircleArrow from '../../assets/icons/circle_arrow_btn.svg';
+
+// Styles
+import './circle-button.scss';
+
+export default function CircleButton({cta, url}) {
+  return (
+    <Link className="circle-button__link" to={url}>
+      <button className="circle-button">
+        <img src={CircleArrow} alt="Arrow" className="circle-button__image"/>
+        <p className="circle-button__text">{cta}</p>
+      </button>
+    </Link>
+  )
+}
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
 
-// Components import
+// Components 
 import Header from '../../components/header/header';
+import CircleButton from '../../components/circle-button/circle-button';
 
+// Images 
+import homeBioPortrait from '../../assets/images/orest_portrait_1.jpg';
+// import stompingGroundDetails from '../../assets/images/StompingGround_Details.jpg';
 
-// Images import
-// import mapImage from '../../Assets/Images/generic_map.jpg'
-
-// Styles import
+// Styles 
 import './home.scss';
 
 export default function Home() {
@@ -19,7 +20,29 @@ export default function Home() {
       <Header />
 
       <section className="home-bio">
-        
+        <img src={homeBioPortrait} alt="Orest" className="home-bio__portrait"/>
+
+        <div className="home-bio__text-container">
+          <p className="home-bio__text">
+          Hi, my name is Orest. I'm a Web Developer proficient in JavaScript, as well as React.
+          </p><p className="home-bio__text">
+          My love of new challenges and creating rich web experiences has led me to transition from Digital Design to Web Development. In February 2021, I earned a diploma in Web Development from tech college BrainStation's bootcamp with a cumulative grade of 89%.
+          </p><p className="home-bio__text">
+          I am an experienced tech industry professional accustomed to working within an Agile Scrum workflow, with skills accumulated working on UI for eCommerce websites and educational platforms. My habit has always been to centralize components, make them versatile and re-usable, and studiously package assets for hand-off.
+          </p><p className="home-bio__text">
+          With a history of producing finely tuned interfaces, my experience manipulating graphics and layout has proven to be a great advantage in modular web design.
+          </p><p className="home-bio__text">
+          The keystones of my practice are reliability, attention to detail, and continuous learning.
+          </p>
+
+          <CircleButton cta="Read more about my history" url="/about" />
+          
+        </div>
+      </section>
+
+      <section className="skills">
+        <h2 className="skills__heading">Skills</h2>
+        <h3 className="skills__sub-heading">Front-End</h3>
       </section>
 
 
