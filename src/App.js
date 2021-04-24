ca87587abba3e10c8079e858319d80b00601bca9
orest-cloud <orest.szopiak@gmail.com>
2021-04-24 16:09:51 -0400

    Added ScrollToTop component

--- src/App.js
+++ src/App.js
@@ -4,17 +4,24 @@ import './styles/global.scss';
 //Modules import
 import React from "react";
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import ReactGA from 'react-ga';
 
 // Pages import
 import About from './pages/about/about.jsx';
 import Home from './pages/home/home.jsx';
 import CV from './pages/cv/cv.jsx';
+import ScrollToTop from './components/scroll-to-top/scroll-to-top.jsx';
 import StompingGround from './pages/stomping-ground/stomping-ground.jsx';
+
+// Initialize Google Analytics
+ReactGA.initialize('UA-000000-01');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 export default function App() {
   return (
 
     <BrowserRouter>
+      <ScrollToTop />
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/about" exact component={About} />
--- src/pages/cv/cv.jsx
+++ src/pages/cv/cv.jsx
@@ -7,7 +7,7 @@ import Footer from '../../components/footer/footer';
 
 // Images 
 import pdfIcon from '../../assets/icons/pdf.svg';
-import pdfImage from '../../assets/images/cv_image.png';
+import {ReactComponent as PdfImage} from '../../assets/images/cv_image.svg';
 
 // Styles 
 import './cv.scss';
@@ -31,10 +31,7 @@ export default class CV extends React.Component {
 
             <a href="/assets/pdfs/OrestSzopiak_CV.pdf" target="_blank" className="cv__link"><img src={pdfIcon} alt="PDF download" className="cv__icon"/>View / Download PDF (73kb)</a>
 
-            <a href="/assets/pdfs/OrestSzopiak_CV.pdf" target="_blank" className="cv__link">
-              <img src={pdfImage} alt="My CV" className="cv__image"/>
-            </a>
-            
+            <PdfImage className="cv__image" />
             
           </div>
         </section>
